d99d67a3a3eda09486033fe6d9d83439b9190db0
jamilet <32970306+jammilet@users.noreply.github.com>
2017-11-28 15:00:07 -0800

    Initial commit

---
+++ Notes/Notes.py
@@ -0,0 +1,32 @@
+print('Hello World')
+
+# jamilet
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()  # creates a blank line
+print('see if you can figure this out')
+print(5 % 3)
+
+# variables
+car_name = 'jamilet mobile'
+car_type = 'toyota'
+car_cylinders = 8
+car_mpg = 9000.1
+
+# inline printing
+print('my car is the %s.' % car_name)
+print('my car is the %s. it is a %s' % (car_name, car_type))
+
+# taking input
+name = input('What is your name?')
+print('Hello %s' % name)
+# print(name)
+
+age = input('What is your age?')
+print('%s you are old' % age)
+
